863f677eb563724c517d4abc19e4047c64408c6a
Asma Siddique <109345779+asma9001@users.noreply.github.com>
2025-04-05 18:02:46 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,23 +63,56 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
-      {googleAnalyticsId ? (
-        <head>
-          <Script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-          ></Script>
-          <Script id="google-anayltics-script">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', '${googleAnalyticsId}');
-          `}
-          </Script>
-        </head>
-      ) : null}
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title ?? 'Asma Portfolio'}</title> {/* Default title */}
+        <meta name="description" content={metadata.description ?? ''} />
+        <meta name="keywords" content={metadata.keywords?.join(', ') ?? ''} />
+        <meta name="creator" content={metadata.creator} />
+        <meta name="theme-color" content={metadata.themeColor[0].color} media={metadata.themeColor[0].media} />
+        <meta name="theme-color" content={metadata.themeColor[1].color} media={metadata.themeColor[1].media} />
+
+        {/* Open Graph */}
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:title" content={metadata.openGraph.title ?? 'Asma Portfolio'} />
+        <meta property="og:description" content={metadata.openGraph.description ?? ''} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title ?? 'Asma Portfolio'} />
+        <meta name="twitter:description" content={metadata.twitter.description ?? ''} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:image" content={metadata.twitter.images} />
+
+        {/* Icons */}
+        <link rel="icon" href={metadata.icons.icon} />
+        <link rel="shortcut icon" href={metadata.icons.shortcut} />
+        <link rel="apple-touch-icon" href={metadata.icons.apple} />
+
+        {googleAnalyticsId && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+            ></Script>
+            <Script id="google-anayltics-script">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${googleAnalyticsId}');
+            `}
+            </Script>
+          </>
+        )}
+      </head>
       <body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
         <Providers>
           <Header />
