52f4c792b58238cbc106293db207b1e77ac05a01
Asma Siddique <109345779+asma9001@users.noreply.github.com>
2025-04-05 21:41:20 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { useEffect } from "react";
 import HeroSection from "@/components/sections/hero";
 import ContactSection from "@/components/sections/contact";
 import AboutMeSection from "@/components/sections/about-me";
@@ -5,8 +7,16 @@ import SkillsSection from "@/components/sections/skills";
 import ExperienceSection from "@/components/sections/experiences";
 import TestimonialsSection from "@/components/sections/testimonials";
 import WorkSection from "@/components/sections/work";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,  // You can change the duration here
+      once: true,  // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
+
   return (
     <>
       <HeroSection />
